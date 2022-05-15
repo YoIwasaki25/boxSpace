ac20f237bad8736e71eff91facf24a4a134460e7
tzResAndDev <iwasaki@typezero.jp>
2022-05-15 17:02:24 +0900

    Box Turn

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
 window.addEventListener('DOMContentLoaded', () => {
-    let cerestial = new Celestial();
-    // cerestial.init();
+    const cerestial = new Celestial();
+    cerestial.animate();
 });
 
 const CELESTIAL_PARAM = {
@@ -13,12 +12,22 @@ const CELESTIAL_PARAM = {
 export default class Celestial {
     private renderer: THREE.WebGLRenderer;
     private clearColor: THREE.Color;
-	private scene: THREE.Scene;
+    private scene: THREE.Scene;
+    private camera: THREE.Camera;
+    private controls: OrbitControls;
+	private geometry: THREE.BoxGeometry;
+	private material: THREE.MeshNormalMaterial;
+	private box: THREE.Mesh;
+	private light: THREE.DirectionalLight;
 
     constructor() {
         this.onResize = this.onResize.bind(this);
+        this.animate = this.animate.bind(this);
+
         this.initRenderer();
-        this.init();
+        this.initScene();
+        this.initCamera();
+		this.initGeometry();
     }
 
     initRenderer = () => {
@@ -41,61 +50,42 @@ export default class Celestial {
             this.renderer.capabilities.isWebGL2 == false &&
             this.renderer.extensions.get('ANGLE_instanced_arrays') == null
         ) {
-			document.getElementById("notSupported").style.display = "";
-			return false;
+            document.getElementById('notSupported').style.display = '';
+            return false;
         }
     };
 
-	initScene = () => {
-		this.scene = new THREE.Scene();
-	}
-    onResize = () => {};
+    initScene = () => {
+        this.scene = new THREE.Scene();
+    };
 
-    init() {
-        const width = 1600;
-        const height = 1600;
+    initCamera = () => {
+        this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 5000);
+        this.camera.position.z = 2000;
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        // 滑らかなコントローラー制御
+        this.controls.enableDamping = true;
+        // 静止までの弾性の調整
+        this.controls.dampingFactor = 0.2;
+    };
 
-        // レンダラーのサイズを設定
-        // this.renderer.setSize(width, height);
-        // this.renderer.setPixelRatio(window.devicePixelRatio);
-        // canvasをbodyに追加
-        // document.body.appendChild(this.renderer.domElement);
-
-        // シーンを作成
-        const scene = new THREE.Scene();
-
-        // カメラを作成
-        const camera = new THREE.PerspectiveCamera(45, 1, 1, 10000);
-        camera.position.set(0, 0, 1000);
-
-        const controls = new OrbitControls(camera, this.renderer.domElement);
-        //滑らかなコントローラー制御
-        controls.enableDamping = true;
-        //静止までの弾性の調整
-        controls.dampingFactor = 0.2;
-
-        // 箱を作成
-        const geometry = new THREE.BoxGeometry(250, 250, 250);
-        const material = new THREE.MeshNormalMaterial();
-        const box = new THREE.Mesh(geometry, material);
-        box.position.z = -5;
-        scene.add(box);
+    initGeometry = () => {
+        this.geometry = new THREE.BoxGeometry(250, 250, 250);
+        this.material = new THREE.MeshNormalMaterial();
+        this.box = new THREE.Mesh(this.geometry, this.material);
+        this.box.position.z = -5;
+        this.scene.add(this.box);
 
         // 平行光源を生成
-        const light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(1, 1, 1);
-        scene.add(light);
+        this.light = new THREE.DirectionalLight(0xffffff);
+        this.light.position.set(1, 1, 1);
+        this.scene.add(this.light);
+    };
 
-        const tick = () => {
-            requestAnimationFrame(tick);
-            controls.update();
-            // box.rotation.x += 0.05;
-            // box.rotation.y += 0.05;
+    onResize = () => {};
 
-            // 描画
-            this.renderer.render(scene, camera);
-        };
-        tick();
-        console.log('Hello Three.js');
-    }
+    animate = () => {
+        requestAnimationFrame(this.animate);
+        this.renderer.render(this.scene, this.camera);
+    };
 }
